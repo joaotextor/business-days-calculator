3879b6c13b0f320c223b3277fe06eeef1d3a456c
João Textor <joaotextor@gmail.com>
2023-06-17 13:05:31 -0300

    feature: check if input value is number

--- functions/validateInput.js
+++ functions/validateInput.js
@@ -2,10 +2,15 @@ import { default as showToast } from "./showToast.js"
 
 const $toast = document.querySelector(".toast")
 
+function isNum(str) {
+    if (typeof str != "string") return false
+    return isNaN(str) && isNaN(parseFloat(str))
+}
+
 export default function validateInput(...fields) {
     let error = 0
     for (let field of fields) {
-        if (!field.value) {
+        if (!field.value || isNum(field.value)) {
             field.classList.add("input-error")
             error = 1
         } else {
