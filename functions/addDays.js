70b94f9b0936584b51b72ee91e053c3c59995d9c
João Textor <joaotextor@gmail.com>
2023-10-05 17:51:29 -0300

    feat: add all cities from RS state

--- functions/addDays.js
+++ functions/addDays.js
@@ -1,18 +1,25 @@
-import { holidays } from "../holidays.js"
+import { holidays } from "../holidays.js";
 
-export default function addDays(date, days) {
-    let newDate = new Date(`${date}T04:00:00.000Z`)
-    let i = 0
-    while (i < days) {
-        newDate.setDate(newDate.getDate() + 1)
-        let brazilDate = newDate.toLocaleDateString("pt-BR", {timeZone: "UTC"})
-        if (newDate.getDay() === 6 || newDate.getDay() === 0 || holidays.includes(brazilDate)) {
-            i = i
-        } else {
-            i++
-        }
+export default function addDays(date, days, secao, subsecao) {
+  let newDate = new Date(`${date}T04:00:00.000Z`);
+  let i = 0;
+  while (i < days) {
+    newDate.setDate(newDate.getDate() + 1);
+    console.log(holidays[secao].cities[subsecao].holidays);
+    let brazilDate = newDate.toLocaleDateString("pt-BR", { timeZone: "UTC" });
+    if (
+      newDate.getDay() === 6 ||
+      newDate.getDay() === 0 ||
+      holidays.national.includes(brazilDate) ||
+      holidays[secao].stateHolidays.includes(brazilDate) ||
+      holidays[secao].cities[subsecao].holidays.includes(brazilDate)
+    ) {
+      i = i;
+    } else {
+      i++;
     }
-    let result = newDate.toLocaleDateString("pt-BR", {timeZone: "UTC"})
-    navigator.clipboard.writeText(result)
-    return result
+  }
+  let result = newDate.toLocaleDateString("pt-BR", { timeZone: "UTC" });
+  navigator.clipboard.writeText(result);
+  return result;
 }
--- index.js
+++ index.js
@@ -1,26 +1,48 @@
-import showToast from "./functions/showToast.js"
-import addDays from "./functions/addDays.js"
+import showToast from "./functions/showToast.js";
+import addDays from "./functions/addDays.js";
 import validateInput from "./functions/validateInput.js";
+import { holidays } from "./holidays.js";
 
-const $form = document.forms["form"]
+const $form = document.forms["form"];
 const $initialDate = document.getElementById("startDate");
 const $daysToAdd = document.getElementById("daysToAdd");
 const $finalDate = document.getElementById("finalDate");
-const $toast = document.querySelector(".toast")
+const $toast = document.querySelector(".toast");
+const $secao = document.getElementById("select-uf");
+const $subsecao = document.getElementById("select-city");
 
-$form.addEventListener("submit", handleFormSubmit)
+document.addEventListener("DOMContentLoaded", loadCities);
+
+$form.addEventListener("submit", handleFormSubmit);
+
+const secao = $secao.value;
+
+function loadCities() {
+  console.log("started loading cities");
+  for (const city in holidays[secao].cities) {
+    const cityName = holidays[secao].cities[city].name;
+    const option = document.createElement("option");
+    option.text = cityName;
+    option.value = city;
+    $subsecao.appendChild(option);
+  }
+}
 
 function handleFormSubmit(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    let oldDate = $initialDate.value
-    let daysToAdd = $daysToAdd.value
+  const oldDate = $initialDate.value;
+  const daysToAdd = $daysToAdd.value;
 
-    if (!validateInput($initialDate, $daysToAdd)) return 0
+  if (!$subsecao.value) {
+    console.log("select a city");
+    return 0;
+  }
 
-    let newDate = addDays(oldDate, daysToAdd)
+  if (!validateInput($initialDate, $daysToAdd)) return 0;
+  let newDate = addDays(oldDate, daysToAdd, $secao.value, $subsecao.value);
 
-    $finalDate.innerText = newDate
+  $finalDate.innerText = newDate;
 
-    showToast($toast, "Data copiada. Use Ctrl+V para colar.", "success", 5000)
+  showToast($toast, "Data copiada. Use Ctrl+V para colar.", "success", 5000);
 }
